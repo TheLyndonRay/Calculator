a006f149d86220a643aa9e1dfe3272412998d38a
Lyndon R. Labayan <lyndon.labayan@gmail.com>
2014-09-29 16:25:48 -0500

    Moved shit around

--- app/src/main/java/com/solaris/lyndon/calculator/MainActivity.java
+++ app/src/main/java/com/solaris/lyndon/calculator/MainActivity.java
@@ -6,16 +6,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
 
-    Button add_button, subtract_btton, multiply_button, divide_button, fact_button;
-
-    TextView result = (TextView)findViewById(R.id.result);
-    TextView firstNumber = (TextView)findViewById(R.id.num1);
-    TextView secondNumber = (TextView)findViewById(R.id.num2);
+    Button add_button, subtract_button, multiply_button, divide_button, fact_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +20,13 @@ public class MainActivity extends Activity {
 
         EventHandler buttonHandler = new EventHandler();
         add_button = (Button)findViewById(R.id.add);
-        subtract_btton = (Button)findViewById(R.id.subtract);
+        subtract_button = (Button)findViewById(R.id.subtract);
         multiply_button = (Button)findViewById(R.id.multiply);
         divide_button = (Button)findViewById(R.id.divide);
         fact_button = (Button)findViewById(R.id.factorial);
 
         add_button.setOnClickListener(buttonHandler);
-        subtract_btton.setOnClickListener(buttonHandler);
+        subtract_button.setOnClickListener(buttonHandler);
         multiply_button.setOnClickListener(buttonHandler);
         divide_button.setOnClickListener(buttonHandler);
         fact_button.setOnClickListener(buttonHandler);
@@ -40,22 +36,47 @@ public class MainActivity extends Activity {
     class EventHandler implements View.OnClickListener {
         @Override
         public void onClick(View v){
+
+            EditText result = (EditText)findViewById(R.id.result);
+            EditText firstNumber = (EditText)findViewById(R.id.num1);
+            EditText secondNumber = (EditText)findViewById(R.id.num2);
+
+            Double answer;
+
             switch (v.getId()){
                 case R.id.add :
 
-                    result.setText("");
+                    answer = Double.valueOf(firstNumber.toString()) + Double.valueOf(secondNumber.toString());
+                    result.setText(answer.toString());
 
                     break;
                 case R.id.subtract :
 
+                    answer = Double.valueOf(firstNumber.toString()) - Double.valueOf(secondNumber.toString());
+                    result.setText(answer.toString());
+
                     break;
                 case R.id.multiply :
+
+                    answer = Double.valueOf(firstNumber.toString()) * Double.valueOf(secondNumber.toString());
+                    result.setText(answer.toString());
 
                     break;
                 case R.id.divide :
 
+                    answer = Double.valueOf(firstNumber.toString()) / Double.valueOf(secondNumber.toString());
+                    result.setText(answer.toString());
+
                     break;
                 case R.id.factorial :
+
+                    Integer input = Integer.valueOf(firstNumber.toString());
+                    Integer factorial = 1;
+                    for (int i = 1; i <= input; i++) {
+                        factorial = factorial * i;
+                    }
+
+                    result.setText(factorial.toString());
 
                     break;
 
